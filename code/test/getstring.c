28ac08ad0c3f8cce9677f125a58b5e2f36a56d04
remotequack <remotequack@gmail.com>
2023-10-06 12:13:35 +0200

    test

--- code/test/getstring.c
+++ code/test/getstring.c
@@ -6,14 +6,9 @@ int main()
 {
     char* test = "test\n";
 
-    int i = 0;
-    for(i = 0; i < 5; i++)
-    {
-        PutChar(test[i]);
-    }
-
     GetString(buf, 5);
 
+    int i = 0;
     for(i = 0; i < 7; i++)
     {
         PutChar(buf[i]);
