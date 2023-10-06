079099ce81c31d75322406ea8eb4cf8c788fc44e
remotequack <remotequack@gmail.com>
2023-10-06 09:47:11 +0200

    quoi

--- code/test/getstring.c
+++ code/test/getstring.c
@@ -8,9 +8,8 @@ int main()
     PutChar('s');
     PutChar('t');
     PutChar('\n');
-    char test1[7];
-    GetString(test1, 7);
-    for(int i = 0; i < 7; i++)
+    GetString((char*)test, 5);
+    for(int i = 0; i < 5; i++)
     {
         PutChar(test[i]);
     }
