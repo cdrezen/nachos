19baef313c003575f058f0b7bf93048222b7b618
cdrezen <corentin.drezen@etu.u-bordeaux.fr>
2023-10-08 17:32:18 +0200

    partie viii et vi fini

--- code/test/printf_test.c
+++ code/test/printf_test.c
@@ -2,6 +2,6 @@
 
 int main()
 {
-    monprintf("blabla %d trucs sont %s. mon char est '%c', hex: %#04x.\n", 3, "machin", '*', '*');
-    Halt();
+    //on appel le printf fait maison dans vsprintf.c
+    return monprintf("blabla %d trucs sont %s. mon char est '%c', hex: %#04x.\n", 3, "machin", '*', '*');
 }
