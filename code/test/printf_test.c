36854678048698deb70fc37c113ef6dbdb1bc9d1
cdrezen <corentin.drezen@etu.u-bordeaux.fr>
2023-10-08 11:53:02 +0200

    printf?

---
+++ code/test/printf_test.c
@@ -0,0 +1,33 @@
+#include "syscall.h"
+#include <stdarg.h>
+
+static char ppbuf[1024];
+
+void monprintf(char* fmt, ...)
+{
+    va_list args;
+	char ch, *bptr;
+	int i;
+
+	va_start(args, fmt);
+	i = vsprintf(ppbuf, fmt, args);
+
+	bptr = ppbuf;
+
+    //PutString(bptr, i);
+
+	while((ch = *(bptr++)) != 0) {
+		if(ch == '\n')
+			PutChar('\r');
+
+		PutChar(ch);
+	}
+
+	va_end(args);
+}
+
+int main()
+{
+    monprintf("blabla %s", "oui");
+    Halt();
+}
