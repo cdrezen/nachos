9a9a14711a9deb49d6571b1443d4bfe068242247
cdrezen <corentin.drezen@etu.u-bordeaux.fr>
2023-10-08 14:21:34 +0200

    return?

--- code/userprog/exception.cc
+++ code/userprog/exception.cc
@@ -84,6 +84,7 @@ void ExceptionHandler(ExceptionType which)
     {
       char str[31];
       int res = machine->ReadRegister(1);
+      machine->ReadMem(res, sizeof(int), &res);
       snprintf(str, 31, "programme termine, retour=%d.\n", res);
       consoledriver->PutString(str);
       interrupt->Powerdown();
