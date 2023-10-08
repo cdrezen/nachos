1e268de7ddf42ec2f0e3eaf3d66b1241232c160b
Kerim CANAKCI <kerim.canakci@etu.u-bordeaux.fr>
2023-10-08 19:18:20 +0200

    test putstring.c

--- code/test/putstring.c
+++ code/test/putstring.c
@@ -2,6 +2,11 @@
 
 int main()
 {
-    PutString("test");//, 4);
+    char buf[300];
+    int i; // Déplacez la déclaration de i en dehors de la boucle
+    for(i = 0; i < 300; i++){
+        buf[i] = 'a';
+    }
+    PutString(buf);
     Halt();
 }
