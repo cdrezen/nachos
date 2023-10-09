fd7420f7b6e7aab7ed8c9aab0124ddc1816d4c94
cdrezen <corentin.drezen@etu.u-bordeaux.fr>
2023-10-09 03:03:56 +0200

    getstring

--- code/test/getstring.c
+++ code/test/getstring.c
@@ -1,15 +1,15 @@
 #include "syscall.h"
 
 //const int TAILLE_LIRE_ECRIRE = 10;
-char buf[10];
+char buf[2000];
 
 int main()
 {
     int i = 0;
 
-    GetString(buf, 800);
+    GetString(buf, 2000);
 
-    for(i = 0; i < 800; i++)
+    for(i = 0; i < 2000; i++)
     {
         PutChar(buf[i]);
     }
