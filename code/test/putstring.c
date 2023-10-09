09345c3aeb868da2b949662fc656ef113d72c7f5
cdrezen <corentin.drezen@etu.u-bordeaux.fr>
2023-10-09 10:26:28 +0200

    make

--- code/test/putstring.c
+++ code/test/putstring.c
@@ -3,14 +3,15 @@
 int main()
 {
     char buf[50];
+    char buf1[2000];
     int i; // Déplacez la déclaration de i en dehors de la boucle
+    
     for(i = 0; i < 50; i++){
         buf[i] = 'a';
     }
 
     PutString(buf);
 
-    char buf1[2000];
     for(i = 0; i < 2000; i++){
         buf1[i] = 'a';
     }
