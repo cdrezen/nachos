7c69412298d8ad554cc888fa74218bfbad5918e9
cdrezen <corentin.drezen@etu.u-bordeaux.fr>
2023-10-09 03:12:36 +0200

    getstring

--- code/test/getstring.c
+++ code/test/getstring.c
@@ -1,15 +1,15 @@
 #include "syscall.h"
 
 #define TAILLE_LIRE_ECRIRE 10
-#define TAILLE_BEAUCOUP_LIRE_ECRIRE 2000
+//#define TAILLE_BEAUCOUP_LIRE_ECRIRE 2000
 
 char buf[TAILLE_LIRE_ECRIRE];
-char bufbcp[TAILLE_BEAUCOUP_LIRE_ECRIRE];
+//char bufbcp[TAILLE_BEAUCOUP_LIRE_ECRIRE];
 
 int main()
 {
     GetString(buf, TAILLE_LIRE_ECRIRE);
     PutString(buf);
-    GetString(bufbcp, TAILLE_BEAUCOUP_LIRE_ECRIRE);
-    PutString(bufbcp);
+    //GetString(bufbcp, TAILLE_BEAUCOUP_LIRE_ECRIRE);
+    //PutString(bufbcp);
 }
