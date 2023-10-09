fa1b96b4d098c4e36ff0e86ff76d2d124f36645e
cdrezen <corentin.drezen@etu.u-bordeaux.fr>
2023-10-09 10:40:09 +0200

    test

--- code/test/putstring.c
+++ code/test/putstring.c
@@ -6,11 +6,11 @@ char bufbcp[TAILLE_BEAUCOUP_LIRE_ECRIRE];
 
 int main()
 {
-    char buf[2000];
+    char buf[1000];
     //char buf1[2000];
     int i; // Déplacez la déclaration de i en dehors de la boucle
 
-    for(i = 0; i < 2000; i++){
+    for(i = 0; i < 1000; i++){
         buf[i] = 'a';
     }
 
