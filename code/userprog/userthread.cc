f919ed2f98f2c567b19a2681810e04880cf217e4
cdrezen <corentin.drezen@etu.u-bordeaux.fr>
2023-10-19 18:20:17 +0200

    ?

--- code/userprog/userthread.cc
+++ code/userprog/userthread.cc
@@ -1,6 +1,7 @@
 #include "system.h"
 #include "synch.h"
 #include "translate.h"
+#include "machine.h"
 
 static void StartUserThread(void *schmurtz)
 {
@@ -13,22 +14,24 @@ static void StartUserThread(void *schmurtz)
 
     printf("StartUserThread   f ptr: %d     arg ptr: %d.\n", f, arg);
     
-    //  for (i = 0; i < NumTotalRegs; i++)
-    //     machine->WriteRegister (i, 0);
+     for (i = 0; i < NumTotalRegs; i++)
+        machine->WriteRegister (i, 0);
 
-    // // Initial program counter -- must be location of the __start function
-    // machine->WriteRegister (PCReg, f);
+    // Initial program counter -- must be location of the __start function
+    machine->WriteRegister (PCReg, f);
 
-    // // Need to also tell MIPS where next instruction is, because
-    // // of branch delay possibility
-    // machine->WriteRegister (NextPCReg, machine->ReadRegister(PCReg) + 4);
+    // Need to also tell MIPS where next instruction is, because
+    // of branch delay possibility
+    machine->WriteRegister (NextPCReg, machine->ReadRegister(PCReg) + 4);
 
-    // // Set the stack register to the end of the address space, where we
-    // // allocated the stack; but subtract off a bit, to make sure we don't
-    // // accidentally reference off the end!
-    // machine->WriteRegister (StackReg, numPages * PageSize - 256);
-    // DEBUG ('a', "Initializing stack register to 0x%x\n",
-    //        numPages * PageSize - 256);
+    // Set the stack register to the end of the address space, where we
+    // allocated the stack; but subtract off a bit, to make sure we don't
+    // accidentally reference off the end!
+    machine->WriteRegister (StackReg, numPages * PageSize - 256);
+
+    DEBUG ('a', "Initializing stack register to 0x%x\n",
+           numPages * PageSize - 256);
+
     delete[] schmurtz;
 }
 
