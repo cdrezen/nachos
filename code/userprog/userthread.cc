8a3b38c1f2e7c7070bebea8638fc03fcee1de7c5
cdrezen <corentin.drezen@etu.u-bordeaux.fr>
2023-10-19 16:58:45 +0200

    startmachin

--- code/userprog/userthread.cc
+++ code/userprog/userthread.cc
@@ -23,11 +23,28 @@ int do_ThreadCreate(int f, int arg)
     
     Thread *t = new Thread(name);
     
-    int schmurtz[2] = {f, arg};
+    void schmurtz[2] = {f, arg};
     t->Start(StarUserThread, schmurltz);
 }
 
 static void StartUserThread(void *schmurtz)
 {
-    
+    int i;
+
+    for (i = 0; i < NumTotalRegs; i++)
+        machine->WriteRegister (i, 0);
+
+    // Initial program counter -- must be location of the __start function
+    machine->WriteRegister (PCReg, schmurtz[0]);
+
+    // Need to also tell MIPS where next instruction is, because
+    // of branch delay possibility
+    machine->WriteRegister (NextPCReg, machine->ReadRegister(PCReg) + 4);
+
+    // Set the stack register to the end of the address space, where we
+    // allocated the stack; but subtract off a bit, to make sure we don't
+    // accidentally reference off the end!
+    machine->WriteRegister (StackReg, numPages * PageSize - 16);
+    DEBUG ('a', "Initializing stack register to 0x%x\n",
+           numPages * PageSize - 16);
 }
