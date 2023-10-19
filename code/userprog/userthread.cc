bc72a6b6b301097c9fab34fe4ebbf9d8b28cb37a
cdrezen <corentin.drezen@etu.u-bordeaux.fr>
2023-10-19 18:08:30 +0200

    fix ptr perdu

--- code/userprog/userthread.cc
+++ code/userprog/userthread.cc
@@ -4,34 +4,36 @@
 
 static void StartUserThread(void *schmurtz)
 {
-    //int* args = (int*)(schmurtz);
+    int* args = (int*)(schmurtz);
 
-    int f = *(int*)(schmurtz);
-    int arg = *(int*)(schmurtz+sizeof(int));
+    int f = args[0];
+    int arg = args[1];
 
     int i;
 
     printf("StartUserThread   f ptr: %d     arg ptr: %d.\n", f, arg);
+    
+    //  for (i = 0; i < NumTotalRegs; i++)
+    //     machine->WriteRegister (i, 0);
 
-/*     for (i = 0; i < NumTotalRegs; i++)
-        machine->WriteRegister (i, 0);
+    // // Initial program counter -- must be location of the __start function
+    // machine->WriteRegister (PCReg, f);
 
-    // Initial program counter -- must be location of the __start function
-    machine->WriteRegister (PCReg, f);
+    // // Need to also tell MIPS where next instruction is, because
+    // // of branch delay possibility
+    // machine->WriteRegister (NextPCReg, machine->ReadRegister(PCReg) + 4);
 
-    // Need to also tell MIPS where next instruction is, because
-    // of branch delay possibility
-    machine->WriteRegister (NextPCReg, machine->ReadRegister(PCReg) + 4);
-
-    // Set the stack register to the end of the address space, where we
-    // allocated the stack; but subtract off a bit, to make sure we don't
-    // accidentally reference off the end!
-    machine->WriteRegister (StackReg, numPages * PageSize - 16);
-    DEBUG ('a', "Initializing stack register to 0x%x\n",
-           numPages * PageSize - 16); */
+    // // Set the stack register to the end of the address space, where we
+    // // allocated the stack; but subtract off a bit, to make sure we don't
+    // // accidentally reference off the end!
+    // machine->WriteRegister (StackReg, numPages * PageSize - 256);
+    // DEBUG ('a', "Initializing stack register to 0x%x\n",
+    //        numPages * PageSize - 256);
+    delete[] schmurtz;
 }
 
 int nameid = 0;
+int* schmurtz;
 
 int do_ThreadCreate(int f, int arg)
 {
@@ -42,6 +44,10 @@ int do_ThreadCreate(int f, int arg)
     
     Thread *t = new Thread(name);
     
-    int schmurtz[2] = {f, arg};
-    t->Start(StartUserThread, (void*)schmurtz);
+    schmurtz = new int[2];
+    schmurtz[0] = f;
+    schmurtz[1] = arg;
+    t->Start(StartUserThread, schmurtz);
+
+    return 0;
 }
