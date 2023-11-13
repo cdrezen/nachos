3044a07e6e2c343e335f663fa82cc0b6c4beabb3
cdrezen <corentin.drezen@etu.u-bordeaux.fr>
2023-11-13 01:37:17 +0100

    philosophes

--- code/test/makethreadsync.c
+++ code/test/makethreadsync.c
@@ -13,6 +13,7 @@ typedef struct
   char* nom;
   int* fourchettes;
   sem_t* sem;
+  sem_t* sem1;
   sem_t* io_sem;
 } simple_args;
 
@@ -21,16 +22,17 @@ void philosophe(void *arg)
     simple_args* args = (struct simple_args*)(arg);
     char* nom = args->nom;
     int* fourchettes = args->fourchettes;
-    sem_t* protege_fourchette = args->sem;
+    sem_t* production_fourchette = args->sem;
+    sem_t* consomation_fourchette = args->sem1;
     sem_t* io_sem = args->io_sem;
 
     while(1)
     {
-      P(protege_fourchette);
-      while((*fourchettes) < 200);// { monprintf(io_sem, "(%s) Oh non, il reste %d fourchettes.\n", nom, *fourchettes); }//attente active
-      (*fourchettes) -= 20;
-      V(protege_fourchette);
-      monprintf(io_sem, "(%s) Miam, il reste %d fourchettes.\n", nom, *fourchettes);
+      P(production_fourchette);
+      //while((*fourchettes) < 200);// { monprintf(io_sem, "(%s) Oh non, il reste %d fourchettes.\n", nom, *fourchettes); }//attente active
+      (*fourchettes) -= 1;
+      V(consomation_fourchette);
+      //monprintf(io_sem, "(%s) Miam, il reste %d fourchettes.\n", nom, *fourchettes);
     }
 }
 
@@ -39,15 +41,16 @@ void fabricant_fourchettes(void* arg)
     simple_args* args = (struct simple_args*)(arg);
     char* nom = args->nom;
     int* fourchettes = args->fourchettes;
-    sem_t* protege_fourchette = args->sem;
+    sem_t* production_fourchette = args->sem;
+    sem_t* consomation_fourchette = args->sem1;
     sem_t* io_sem = args->io_sem;
 
     while(1)
     {
-      P(protege_fourchette);
+      P(consomation_fourchette);
       (*fourchettes) += 1;
-      V(protege_fourchette);
-      //monprintf(io_sem, "(%s) Tenez, voici 1 fouchette, il reste %d fourchettes.\n", nom, *fourchettes);
+      V(production_fourchette);
+      monprintf(io_sem, "(%s) Tenez, voici 1 fouchette, il reste %d fourchettes.\n", nom, *fourchettes);
     }
 }
 
@@ -56,19 +59,21 @@ const int FOURCHETTES_MIN = 1;
 int main ()
 {
     int fourchettes = 0;
-    sem_t protege_fourchette = { -1 };
+    sem_t production_fourchette = { -1 };
+    sem_t consomation_fourchette = { -1 };
     sem_t io_sem = { -1 };
 
-    simple_args fabricant = { "Fabricant", &fourchettes, &protege_fourchette, &io_sem };
-    simple_args nietzsche = { "Nietzsche", &fourchettes, &protege_fourchette, &io_sem };
-    simple_args aristote = { "Aristote", &fourchettes, &protege_fourchette, &io_sem };
+    simple_args fabricant = { "Fabricant", &fourchettes, &production_fourchette, &consomation_fourchette, &io_sem };
+    simple_args nietzsche = { "Nietzsche", &fourchettes, &production_fourchette, &consomation_fourchette, &io_sem };
+    simple_args aristote = { "Aristote", &fourchettes, &production_fourchette, &consomation_fourchette, &io_sem };
 
-    sem_t_init(&protege_fourchette, FOURCHETTES_MIN);
+    sem_t_init(&production_fourchette, 0);
+    sem_t_init(&consomation_fourchette, 10);
     sem_t_init(&io_sem, 1);
 
     ThreadCreate(fabricant_fourchettes, &fabricant);
     ThreadCreate(philosophe, &nietzsche);
-    ThreadCreate(philosophe, &aristote);
+    //ThreadCreate(philosophe, &aristote);
 
     ThreadExit();
 }
